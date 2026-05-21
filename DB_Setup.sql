cc06589c5dbeee5b349c553f46a75528f4783b4b
Vishwajeet Kore <123283352+vishwajeetkore@users.noreply.github.com>
2026-05-21 23:19:59 +0530

    Update DB_Setup.sql

--- DB_Setup.sql
+++ DB_Setup.sql
@@ -1,15 +1,15 @@
 -- -----------------------------------------------------
 -- Schema fullstack
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `vishwajeetDB`;
+DROP SCHEMA IF EXISTS `vishwajeet_ecomm`;
 
-CREATE SCHEMA `vishwajeetDB`;
-USE `vishwajeetDB` ;
+CREATE SCHEMA `vishwajeet_ecomm`;
+USE `vishwajeet_ecomm` ;
 
 -- -----------------------------------------------------
 -- Table `fullstack`.`product_category`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `vishwajeetDB`.`product_category` (
+CREATE TABLE IF NOT EXISTS `vishwajeet_ecomm`.`product_category` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `category_name` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
@@ -19,7 +19,7 @@ AUTO_INCREMENT = 1;
 -- -----------------------------------------------------
 -- Table `fullstack`.`product`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `vishwajeetDB`.`product` (
+CREATE TABLE IF NOT EXISTS `vishwajeet_ecomm`.`product` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(255) DEFAULT NULL,
   `name` VARCHAR(255) DEFAULT NULL,
